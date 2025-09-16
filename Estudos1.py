3f6c9fed937e9e5c760e99ce0f7212d015777a29
Vituuuw <anotherharusunrise@gmail.com>
2025-09-16 19:07:31 -0300

    Add files via upload

---
+++ Estudos1.py
@@ -0,0 +1,6 @@
+#tudo que tiver na frente da HASHTAG nao sera lido pelo comando e é chamado de comentarios de codigos
+#posso usar para dizer para que serve cada linha de codigo
+
+"""posso usar tambem 3 aspas normais e aspas simples tambem, sao chamados de DockString
+diferente das hashtag que so da pra por em uma linha, esse da pra por em várias,
+porém, o programa le esse codigo, melhor ficar atento a isso"""
